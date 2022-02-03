dcbaf6fb87a75b168957ba242ffff2a8fa267b4b
BadreddineZatout <hb_zatout@esi.dz>
2022-02-03 16:03:23 +0100

    replaced react bootstrap with tailwind & headless ui

--- ecomm-dashboard/src/components/layout/Header.js
+++ ecomm-dashboard/src/components/layout/Header.js
@@ -1,20 +1,82 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Disclosure } from '@headlessui/react'
+import {  MenuIcon, XIcon } from '@heroicons/react/outline'
 
-function Header() {
-  return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">E-Commerce</Navbar.Brand>
-        <Nav className="me-auto navbar-wrapper">
-          <Link to='/'>Home</Link>
-          <Link to="/products">Products</Link>
-          <Link to="/login">Log in</Link>
-          <Link to="/register">Register</Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Products', href: '/products', current: false },
+  { name: 'Login', href: '/login', current: false },
+  { name: 'Register', href: '/register', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Header;
+export default function Example() {
+  return (
+    <Disclosure as="nav" className="bg-Black">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-centre sm:justify-between">
+                <div className="flex-shrink-0 flex items-center">
+                  <h3 className='text-white font-bold cursor-pointer'>E-Commerce</h3>
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          'text-Orange hover:bg-gray-900 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-semibold no-underline'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                      {/* TODO - add redux to manage current  */}
+                      {/* item.current ? 'bg-gray-900 text-Blue' :  */}
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
---
+++ ecomm-dashboard/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/**/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        Orange: "#FEAE47",
+        Blue: "#56BDE6",
+        Black: "#434852",
+      },
+    },
+  },
+  plugins: [],
+};
