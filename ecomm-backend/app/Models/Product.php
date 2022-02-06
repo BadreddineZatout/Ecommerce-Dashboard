1ccb25924ae6ee3e2249d918c6e4c1d1a21e5bfe
BadreddineZatout <hb_zatout@esi.dz>
2022-02-06 12:17:34 +0100

    add laravel media for product image

---
+++ ecomm-backend/app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Product extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable = ['name', 'description', 'price', 'user_id'];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('products');
+    }
+}
