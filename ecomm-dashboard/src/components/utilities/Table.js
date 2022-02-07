14b893a6cc1d828d8b8a36efb65c13dc097cb2db
BadreddineZatout <hb_zatout@esi.dz>
2022-02-07 14:23:39 +0100

    fix some design

--- ecomm-dashboard/src/components/utilities/Table.js
+++ ecomm-dashboard/src/components/utilities/Table.js
@@ -1,6 +1,6 @@
 export default function Table({ products }) {
   return (
-    <div className="flex flex-col mt-36">
+    <div className="flex flex-col mt-10">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:mx-32">
         <div className="py-2 align-middle inline-block min-w-full  sm:px-6 lg:px-8">
           <div className="flex justify-between items-center">
