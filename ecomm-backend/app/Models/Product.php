00c3417368ac849265b938e94f7f25626b4d99e5
BadreddineZatout <hb_zatout@esi.dz>
2022-02-09 22:54:43 +0100

    define relationship between product & user

--- ecomm-backend/app/Models/Product.php
+++ ecomm-backend/app/Models/Product.php
@@ -17,4 +17,9 @@ class Product extends Model implements HasMedia
     {
         $this->addMediaCollection('products');
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- ecomm-backend/app/Models/User.php
+++ ecomm-backend/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -32,7 +31,8 @@ class User extends Authenticatable
         'password',
         'remember_token',
         'created_at',
-        'updated_at'
+        'updated_at',
+        'email_verified_at',
     ];
 
     /**
@@ -43,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
