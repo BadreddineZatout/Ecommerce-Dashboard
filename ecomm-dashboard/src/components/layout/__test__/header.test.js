2660a1e870744982274fb000db4d1adadbae8c01
BadreddineZatout <hb_zatout@esi.dz>
2022-02-12 18:26:38 +0100

    header component test

---
+++ ecomm-dashboard/src/components/layout/__test__/header.test.js
@@ -0,0 +1,59 @@
+import { getAllByRole, render, screen } from "@testing-library/react";
+
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "../Header";
+import store, { hideAuth, login, showProducts } from "../../../features";
+
+const persistor = persistStore(store);
+
+it("renders header when user is not logged", () => {
+  render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
+  const home = screen.getByText("Home");
+  const login = screen.getByText("Login");
+  const logout = screen.getByText("Register");
+  expect(home).toBeInTheDocument();
+  expect(login).toBeInTheDocument();
+  expect(logout).toBeInTheDocument();
+});
+
+it("renders header when user is logged", () => {
+  store.dispatch(
+    login({
+      user: {
+        id: "1",
+        name: "badreddine zatout",
+        email: "hb_zatout@esi.dz",
+      },
+    })
+  );
+  store.dispatch(hideAuth());
+  store.dispatch(showProducts());
+
+  render(
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  );
+  const home = screen.getByText("Home");
+  const products = screen.getByText("Products");
+  const userLink = screen.getByText(store.getState().user.user.name);
+  expect(home).toBeInTheDocument();
+  expect(products).toBeInTheDocument();
+  expect(userLink).toBeInTheDocument();
+});
